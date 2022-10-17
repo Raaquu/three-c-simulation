1a3d8e1943e822b57d51940f0ccac8cafa2edcb3
Christopher Lösch <c.loesch@tu-bs.de>
2022-10-17 03:56:06 +0200

    toml

--- codes/svelte/three-c-simulation/src/jscript/yamlUpload/stores.js
+++ codes/svelte/three-c-simulation/src/jscript/yamlUpload/stores.js
@@ -1,4 +1,4 @@
-//import { writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const submitted = writable(false)
 export const fileText = writable([])
@@ -9,37 +9,3 @@ export const yamlData = writable([])
 export const correct = writable(false)
 export const dispZero = writable(true)
 export const showOtherForm = writable(true)
-
-    /**
-
-    < !-- import {
-        AxesHelper,
-        BoxBufferGeometry,
-        GridHelper,
-        MeshStandardMaterial,
-    } from "three";
-import {
-    Canvas,
-    DirectionalLight,
-    HemisphereLight,
-    OrbitControls,
-    PerspectiveCamera,
-    Mesh,
-    Object3DInstance,
-} from "@threlte/core"; -->
-
-    	< !--import {
-    Mesh,
-    Object3DInstance,
-} from "@threlte/core";
-import { correct, dataPos, dispZero } from './jscript/yamlUpload/stores.js' -->
-    	< !--import {
-    Mesh,
-    Object3DInstance,
-} from "@threlte/core";
-import { correct, dataNeg, dispZero } from './jscript/yamlUpload/stores.js' -->
-    	< !--import {
-    Mesh,
-    Object3DInstance,
-} from "@threlte/core";
-import { correct, dataZero, dispZero } from './jscript/yamlUpload/stores.js' --> */
