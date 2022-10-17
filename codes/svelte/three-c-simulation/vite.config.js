291ef3d2ce6d722cfc2ea04f5602a63ee3b5aaca
Christopher Lösch <c.loesch@tu-bs.de>
2022-10-17 03:06:35 +0200

    toml

--- codes/svelte/three-c-simulation/vite.config.js
+++ codes/svelte/three-c-simulation/vite.config.js
@@ -33,7 +33,7 @@ const defineConfig ={
 	  target: "es2020",
 	  module: "es2020",
 	  rollupOptions: {
-		// plugins: [nodePolyfills({ crypto: true })],
+		plugins: [nodePolyfills({ crypto: true })],
 		external: ['numjs', 'mathjs', 'js-yaml', 'three'],
 	  },
 	}
