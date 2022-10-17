e9e5d0c1d710662ed4910cd72da06c16359af0d3
Christopher Lösch <c.loesch@tu-bs.de>
2022-10-17 04:04:20 +0200

    toml

--- codes/svelte/three-c-simulation/svelte.config.js
+++ codes/svelte/three-c-simulation/svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	},
 	// Trying to change the global config of vite, not working
 	vite: {
